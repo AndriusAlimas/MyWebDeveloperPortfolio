b6b9ca1a29fc98fd786488d95d094cd00b32fae0
Andrius Alimas <alimaswebdeveloper@gmail.com>
2025-04-09 20:41:45 +0100

    Adding skills technical bars in html structure

--- js/myScript.js
+++ js/myScript.js
@@ -1,5 +1,6 @@
 const resumeList = document.querySelectorAll(".resume-list");
 const resumeBoxs = document.querySelectorAll(".resume-box");
+
 // navbar actions and all section actions along with cube rotation when navbar is clicked
 
 // resume section when clicking tab-list
@@ -12,6 +13,18 @@ resumeList.forEach((list, indx) => {
     resumeBoxs[indx].classList.add("active");
   });
 });
+
+// Function to activate the skills tab and show the skills section
+function activateTab(tabIndex) {
+  const allTabs = document.querySelectorAll(".resume-box");
+  allTabs.forEach((tab, index) => {
+    tab.classList.remove("active"); // Hide all resume boxes
+    if (index === tabIndex) {
+      tab.classList.add("active"); // Show the skills tab
+    }
+  });
+}
+
 // portfolio section when clicking tab-list
 
 // visibility for contact section when reloading (cube reloading animation)
