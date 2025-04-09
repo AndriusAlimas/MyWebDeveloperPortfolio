b188d7a16aee24e3456f68bd6782fd840fe34566
Andrius Alimas <alimaswebdeveloper@gmail.com>
2025-04-09 14:36:18 +0100

    added functionality to tab list items when press added class active and remove class previous who has active

--- js/myScript.js
+++ js/myScript.js
@@ -1,7 +1,14 @@
+const resumeList = document.querySelectorAll(".resume-list");
+
 // navbar actions and all section actions along with cube rotation when navbar is clicked
 
 // resume section when clicking tab-list
-
+resumeList.forEach((list, indx) => {
+  list.addEventListener("click", () => {
+    document.querySelector(".resume-list.active").classList.remove("active");
+    list.classList.add("active");
+  });
+});
 // portfolio section when clicking tab-list
 
 // visibility for contact section when reloading (cube reloading animation)
